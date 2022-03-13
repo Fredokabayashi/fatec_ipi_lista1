476ccb81e6e2c65c54f5138a3c96f684c975c438
Fred Okabayashi <frederico.okabayashi@fatec.sp.gov.br>
2022-03-13 00:33:49 -0300

    Commit Ex03

---
+++ lista_2/Ex03.java
@@ -0,0 +1,40 @@
+/* 3. Ler três valores reais e exibir o maior valor entre eles. 
+Suponha que eles sejam diferentes. */
+
+public class Ex03 {
+    public static void main(String[] args) {
+        
+	
+            //chama e/ou cria Scanner para obter dados a partir da janela de comando
+            Scanner input = new Scanner(System.in);
+            
+            int number1;
+            int number2;
+            int number3;
+            
+            int soma;
+            int max;
+            int mim;
+            int numero3 = 0;
+            
+            System.out.print("Digite o primeiro número:   \n");
+            number1 = input.nextInt();
+            
+            System.out.print("Digite o segundo número:    \n");
+            number2 = input.nextInt();
+            
+            System.out.print("Digite o terceiro número:   \n");
+            number3 = input.nextInt();
+            
+             if (number1 > number2)
+                  number1 = numero3;
+                  number2 = numero3;
+                  else
+                      number1 = 
+            
+            
+        
+        }
+    }
+    
+}
